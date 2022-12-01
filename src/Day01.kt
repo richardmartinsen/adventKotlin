5c4f3bc3a26e7e8ae9303b9a43c10edae6d46752
Richard Martinsen <github@martinsen.org>
2022-12-01 13:19:20 +0100

    Solve Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,43 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.foldIndexed(Pair(listOf<List<Int>>(), listOf<Int>())) { idx, acc, line ->
+            when {
+                idx + 1 == input.size -> {
+                    Pair(acc.first + listOf(acc.second + line.toInt()), listOf())
+                }
+
+                line.isEmpty() -> {
+                    Pair(acc.first + listOf(acc.second), listOf())
+                }
+
+                else -> {
+                    Pair(acc.first, acc.second + line.toInt())
+                }
+            }
+        }.first.maxOf { list ->
+            list.sumOf { it }
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        return input.foldIndexed(Pair(listOf<List<Int>>(), listOf<Int>())) { idx, acc, line ->
+            when {
+                idx + 1 == input.size -> {
+                    Pair(acc.first + listOf(acc.second + line.toInt()), listOf())
+                }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+                line.isEmpty() -> {
+                    Pair(acc.first + listOf(acc.second), listOf())
+                }
+
+                else -> {
+                    Pair(acc.first, acc.second + line.toInt())
+                }
+            }
+        }.first.map { list ->
+            list.sumOf { it }
+        }.sortedDescending().take(3).sum()
+    }
 
     val input = readInput("Day01")
     println(part1(input))
